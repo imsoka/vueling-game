8a2177a8e04b7b47a806dffebb45f5d7d5867451
Victor Araque Casaus <varaquecasaus@gmail.com>
2023-05-13 04:03:59 +0200

    feat(project-structure): super structured project

--- api/routes.go
+++ api/routes.go
@@ -6,6 +6,8 @@ func SetRoutes() {
 	//Main route, front page, opens websocket for client
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 
+	//Join game
+
 	//Get users scores
 
 	//Click the button, update score
